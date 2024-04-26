ea78f794c12a76aa15a758c6459fc4bfb3874250
JeanBaptisteCheramy <jeanbaptiste.cheramy@gmail.com>
2024-04-26 15:35:32 +0200

    register WIP

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -8,58 +8,85 @@ type AuthProps = {
 };
 
 export default function AuthForm(props: AuthProps) {
-  const inputStyle = "m-auto focus:outline-none focus:ring-2 ring-tertiary rounded-md p-2 w-5/6 placeholder:text-center placeholder:italic placeholder:opacity-60";
+  const inputStyle =
+    "m-auto focus:outline-none focus:ring-2 ring-tertiary rounded-md p-2 w-5/6 placeholder:text-center placeholder:italic placeholder:opacity-60";
+
+  async function POST(dataToWrite) {
+    'use server'
+    const res = await fetch("http://localhost:3333/api/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToWrite),
+    });
+
+    const data = await res.json();
+    console.log(data);
+    
+    return Response.json(data);
+  }
+  const register = async (formData: FormData) => {
+    "use server";
+    const data = Object.fromEntries(formData);
+    POST(data)
+  };
 
   return (
     <form
-      className={`${props.type === "register" ? "bg-secondary rounded-l-2xl" : "bg-tertiary rounded-r-2xl"} flex flex-col gap-8 py-4`}
+      className={`${props.type === "register" ? "bg-secondary rounded-l-2xl" : "bg-tertiary rounded-r-2xl"} flex flex-col gap-8 justify-between py-4`}
+      action={register}
     >
-      <input
-        type="text"
-        name="email"
-        placeholder="Email"
-        className={inputStyle}
-      />
-      {props.type === "register" && (
+      <div className="flex flex-col gap-4 ">
         <input
           type="text"
-          name="firstName"
-          placeholder="FirstName"
+          name="email"
+          placeholder="Email"
           className={inputStyle}
         />
-      )}
-      {props.type === "register" && (
-        <input
-          type="text"
-          name="lastName"
-          placeholder="LastName"
-          className={inputStyle}
-        />
-      )}
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        className={inputStyle}
-      />
-      {props.type === "register" && (
+        {props.type === "register" && (
+          <input
+            type="text"
+            name="firstName"
+            placeholder="FirstName"
+            className={inputStyle}
+          />
+        )}
+        {props.type === "register" && (
+          <input
+            type="text"
+            name="lastName"
+            placeholder="LastName"
+            className={inputStyle}
+          />
+        )}
         <input
           type="password"
-          name="password_confirmation"
-          placeholder="Confirm password"
+          name="password"
+          placeholder="Password"
           className={inputStyle}
         />
-      )}
-      <button
-        type="submit"
-        className={
-          props.type === "register"
-            ? "bg-tertiary text-primary rounded-md w-1/2 m-auto h-12 shadow-own-1"
-            : "text-tertiary bg-secondary rounded-md w-1/2 m-auto h-12 shadow-own-1"
-        }
-      >
-        {props.type === "login" ? "Login" : "Register"}
-      </button>
+        {props.type === "register" && (
+          <input
+            type="password"
+            name="password_confirmation"
+            placeholder="Confirm password"
+            className={inputStyle}
+          />
+        )}
+      </div>
+      <div className="flex justify-center">
+        <button
+          type="submit"
+          className={
+            props.type === "register"
+              ? "bg-tertiary text-primary rounded-md w-1/2 m-auto h-12 shadow-own-1"
+              : "text-tertiary bg-secondary rounded-md w-1/2 m-auto h-12 shadow-own-1"
+          }
+        >
+          {props.type === "login" ? "Login" : "Register"}
+        </button>
+      </div>
     </form>
   );
 }
